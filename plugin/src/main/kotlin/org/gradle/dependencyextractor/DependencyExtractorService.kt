69ca3878ff6cb98c3753e61636a8dd3495e5670f
Daz DeBoer <daz@gradle.com>
2023-03-04 12:59:48 -0700

    Add service that prints all resolved dependencies

---
+++ plugin/src/main/kotlin/org/gradle/dependencyextractor/DependencyExtractorService.kt
@@ -0,0 +1,117 @@
+package org.gradle.dependencyextractor
+
+import org.gradle.api.artifacts.component.ComponentIdentifier
+import org.gradle.api.artifacts.result.ResolvedComponentResult
+import org.gradle.api.artifacts.result.ResolvedDependencyResult
+import org.gradle.api.internal.artifacts.configurations.ResolveConfigurationDependenciesBuildOperationType
+import org.gradle.internal.operations.*
+import java.net.URI
+
+class DependencyExtractorService :
+    BuildOperationListener,
+    AutoCloseable {
+
+    init {
+        println("Creating: DependencyExtractorService")
+    }
+
+    override fun started(buildOperation: BuildOperationDescriptor, startEvent: OperationStartEvent) {
+        // This method will never be called when registered in a `BuildServiceRegistry` (ie. Gradle 6.1 & higher)
+        // No-op
+        println("started: ${buildOperation.id}")
+    }
+
+    override fun progress(operationIdentifier: OperationIdentifier, progressEvent: OperationProgressEvent) {
+        // This method will never be called when registered in a `BuildServiceRegistry` (ie. Gradle 6.1 & higher)
+        // No-op
+        println("progress: $operationIdentifier")
+    }
+
+    override fun finished(buildOperation: BuildOperationDescriptor, finishEvent: OperationFinishEvent) {
+        handleBuildOperationType<
+            ResolveConfigurationDependenciesBuildOperationType.Details,
+            ResolveConfigurationDependenciesBuildOperationType.Result,
+            ResolveConfigurationDependenciesBuildOperationType
+            >(buildOperation, finishEvent) { details, result -> extractDependencies(details, result) }
+    }
+
+    private fun extractDependencies(
+        details: ResolveConfigurationDependenciesBuildOperationType.Details,
+        result: ResolveConfigurationDependenciesBuildOperationType.Result
+    ) {
+        val repositoryLookup = RepositoryUrlLookup(details, result)
+        val rootComponent = result.rootComponent
+
+        println()
+        println("RESOLVED: ${details.buildPath} | ${rootComponent.id} | ${details.configurationName}")
+        walkResolvedComponentResult(rootComponent, repositoryLookup, "", mutableSetOf())
+    }
+
+    private fun walkResolvedComponentResult(
+        component: ResolvedComponentResult,
+        repositoryLookup: RepositoryUrlLookup,
+        prefix: String,
+        seenComponents: MutableSet<ComponentIdentifier>
+    ) {
+        component.dependencies
+            .filterIsInstance<ResolvedDependencyResult>()
+            .forEach {
+                val repositoryUrl = repositoryLookup.doLookup(it)
+                val targetComponent = it.selected
+
+                if (seenComponents.add(targetComponent.id)) {
+                    println("${prefix}Dep: ${targetComponent.id} [${repositoryUrl}]")
+                    walkResolvedComponentResult(targetComponent, repositoryLookup, "$prefix-", seenComponents)
+                } else {
+                    println("${prefix}Dep: ${targetComponent.id} [SEEN]")
+                }
+            }
+    }
+
+    private class RepositoryUrlLookup(
+        private val details: ResolveConfigurationDependenciesBuildOperationType.Details,
+        private val result: ResolveConfigurationDependenciesBuildOperationType.Result
+    ) {
+
+        private fun getRepositoryUrlForId(id: String): String? {
+            return details
+                .repositories
+                ?.find { it.id == id }
+                ?.properties
+                ?.let { it["URL"] as? URI }
+                ?.toURL()
+                ?.toString()
+        }
+
+        /**
+         * Looks up the repository for the given [ResolvedDependencyResult].
+         */
+        fun doLookup(resolvedDependencyResult: ResolvedDependencyResult): String? {
+            // Get the repository id from the result
+            val repositoryId = result.getRepositoryId(resolvedDependencyResult.selected)
+            return repositoryId?.let { getRepositoryUrlForId(it) }
+        }
+    }
+
+    override fun close() {
+    }
+}
+
+private inline fun <reified D, reified R, BT : BuildOperationType<D, R>> handleBuildOperationType(
+    buildOperation: BuildOperationDescriptor,
+    finishEvent: OperationFinishEvent,
+    handler: (details: D, result: R) -> Unit
+) {
+    val details: D? = buildOperation.details.let {
+        if (it is D) it else null
+    }
+    val result: R? = finishEvent.result.let {
+        if (it is R) it else null
+    }
+    if (details == null && result == null) {
+        return
+    } else if (details == null || result == null) {
+        throw IllegalStateException("buildOperation.details & finishedEvent.result were unexpected types")
+    }
+    handler(details, result)
+}
--- plugin/src/main/kotlin/org/gradle/dependencyextractor/DependencyExtractorSettingsPlugin.kt
+++ plugin/src/main/kotlin/org/gradle/dependencyextractor/DependencyExtractorSettingsPlugin.kt
@@ -2,11 +2,21 @@ package org.gradle.dependencyextractor
 
 import org.gradle.api.Plugin
 import org.gradle.api.initialization.Settings
+import org.gradle.api.internal.GradleInternal
+import org.gradle.api.provider.Provider
+import org.gradle.api.provider.ProviderFactory
+import org.gradle.internal.build.event.BuildEventListenerRegistryInternal
 
 class DependencyExtractorSettingsPlugin: Plugin<Settings> {
     override fun apply(settings: Settings) {
         println("Applied dependency extractor plugin to Settings '${settings.rootProject.name}'")
-        settings.gradle.beforeProject {
+        val gradle = settings.gradle as GradleInternal
+
+        val dependencyExtractorService = DependencyExtractorService()
+        val dependencyExtractorServiceProvider = gradle.services.get(ProviderFactory::class.java).provider { dependencyExtractorService }
+        gradle.services.get(BuildEventListenerRegistryInternal::class.java).onOperationCompletion(dependencyExtractorServiceProvider)
+
+        gradle.beforeProject {
             it.plugins.apply(DependencyExtractorProjectPlugin::class.java)
         }
     }
